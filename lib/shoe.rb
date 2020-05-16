104b72d47ade97ba844dbb9676b140f60fa21b64
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-16 16:08:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,5 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
 
   BRANDS = []
 
@@ -14,7 +13,7 @@ class Shoe
   end
 
   def brand=(brand)
-    @brand = brand.uniq
+    @brand = brand
     BRANDS << brand
   end
 
