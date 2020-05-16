a54f751b633a33b148fc6877a386f7fc5a3d280d
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-16 16:10:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
@@ -12,6 +14,7 @@ class Shoe
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+
 
   def brand=(brand)
     @brand = brand
